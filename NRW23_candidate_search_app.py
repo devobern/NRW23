794cade6c72a447c2554afa851c184aedc7b9afd
Nicolin Dora <mail@nicolin-dora.ch>
2023-10-29 08:54:40 +0100

    Initial commit with all the needed files.

---
+++ NRW23_candidate_search_app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+import zipfile
+import json
+import traceback
+
+app = Flask(__name__)
+
+ZIP_FILE_PATH = "./files/NRW2023-kandidierende.zip"
+JSON_FILENAME = "NRW2023-kandidierende.json"
+
+def get_data_from_zip():
+    with zipfile.ZipFile(ZIP_FILE_PATH, 'r') as z:
+        with z.open(JSON_FILENAME) as f:
+            return json.load(f)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    first_name = request.form.get('first_name').lower()
+    last_name = request.form.get('last_name').lower()
+
+    try:
+        data = get_data_from_zip()
+        candidates = data['level_kantone']
+
+        for person in candidates:
+            if person['vorname'].lower() == first_name and person['name'].lower() == last_name:
+                return render_template('result.html', person=person)
+
+        return "Person nicht gefunden", 404
+
+    except Exception as e:
+        tb = traceback.format_exc()
+        return f"Fehler: {e}\n\nTraceback:\n{tb}", 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
