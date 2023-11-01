611a487b0689841ccac3f5be9b5a40161058634d
Nicolin Dora <mail@nicolin-dora.ch>
2023-11-01 16:09:52 +0100

    Removes dev run cofnig

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from app import app
 
 if __name__ == '__main__':
-    # todo: Remove after testing: 
-    app.run(host='0.0.0.0', port=5000, ssl_context=('/home/nicolin/Projects/local_certs/localhost+2.pem', '/home/nicolin/Projects/local_certs/localhost+2-key.pem'))
-    #app.run()
+    app.run()
